e17dd5c186bae5a1237a50c3b6e4a1e6563caf37
Cornelii <onelife0985@gmail.com>
2020-08-22 11:37:47 +0900

    Neural Net Since i graduated

--- ML_in_Python/utils.py
+++ ML_in_Python/utils.py
@@ -2,14 +2,20 @@ import numpy as np
 
 
 def sigmoid(x):
+    if type(x) == list:
+        x = np.array(x)
     return 1 / (1 + np.exp(-x))
 
 
 def relu(x):
+    if type(x) == list:
+        x = np.array(x)
     return np.maximum(0, x)
 
 
 def softmax(x):
+    if type(x) == list:
+        x = np.array(x)
     exp_x = np.exp(x - np.max(x))
     return exp_x / np.sum(exp_x)
 
@@ -29,7 +35,7 @@ def mean_square_error(y, t):
     return 0.5 * np.sum((y-t)**2)
 
 
-def numerical_diff(f,x):
+def numerical_diff(f, x):
     if type(x) != np.ndarray:
         x = np.array(x)
     h = 5e-5
@@ -38,26 +44,65 @@ def numerical_diff(f,x):
 
 def numerical_gradient(f, x):
     h = 5e-5
+    if type(x) != np.ndarray:
+        x = np.array(x)
+    return (f(x+h) - f(x-h)) / (2*h)
 
 
-
-class Layer:
+class Dense:
 
     def __init__(self, num_input, num_output, activation_layer=sigmoid):
         self.x_len = num_input
         self.y_len = num_output
-        self.w = np.ones([num_input, num_output], np.float64)
-        self.b = np.zeros([1, num_output], np.float64)
+        self.W = np.ones([num_input, num_output], np.float64)
+        self.B = np.zeros([1, num_output], np.float64)
         self.activation_layer = activation_layer
+        self.dW = np.zeros_like(self.W) # initialization necessary
+        self.dB = np.zeros_like(self.B)
 
         # initialization
         # normal distribution
     def forward(self, x):
-        y = np.dot(x, self.w) + self.b
+        y = np.dot(x, self.W) + self.B
         return self.activation_layer(y)
 
     def backward(self, y):
         pass
+
+    def get_gradients(self, y):
+        pass
+
+
+class Optimizer:
+    def __init__(self, learning_rate=0.001):
+        self.learning_rate = learning_rate
+
+    def update(self, dw, w):
+        w -= self.learning_rate * dw
+
+
+class Momentum(Optimizer):
+
+    def __init__(self, learning_rate=0.001, alpha=0.99):
+        super().__init__(learning_rate)
+        self.alpha = 0.99
+
+    def update(self, dw, w):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
